cb6d271d1c772584ab4ba337c8698cf20731837b
Stateless-X <purin.buriwong@gmail.com>
2024-08-12 00:13:26 +0700

    Allows additional fields for entry updates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,6 +42,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price, :status, :imgUrl)
     end
 end
